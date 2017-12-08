6e670248fd92083b91cc61f6d8a7e23c92cb7d3c
Jan Zarsky <jan.zarsky@gmail.com>
2017-12-08 12:50:24 +0100

    add authentication

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Test;
+namespace App\Question;
+namespace App\TestQuestion;
+namespace App\Animal;
+namespace App\QuestionAnimal;
+namespace App\User;
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        return "asdf";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/tests', ['as' => 'tests',
+        'uses' => 'TestController@index']);
 });
+
+//Route::group(['middleware' => ['auth', 'admin']], function() {
+//});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
