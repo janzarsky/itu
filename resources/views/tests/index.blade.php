25e824dd4aa5e82f4f520a66a8ea30f44528d236
Jan Zarsky <jan.zarsky@gmail.com>
2017-12-09 16:10:20 +0100

    redesign test page

--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -1,45 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container main-container">
     <div class="row">
         <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Tests</div>
-
-                <div class="panel-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Test</th>
-                                <th scope="col">Obtížnost</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($tests as $t)
-                            <tr>
-                                <td>{{ $t->name }}</td>
-                                <td>
-                                @if ($t->difficulty === 1)
-                                    Lehká
-                                @elseif ($t->difficulty === 2)
-                                    Střední
-                                @else
-                                    Těžká
-                                @endif
-                                </td>
-                                <td>
-                                    <a href="{{ route('tests.take', ['id' => $t->id]) }}"
-                                        class="btn btn-primary">Spustit test</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+            <h2>Testy</h2>
+        </div>
+        
+    @foreach ($tests as $t)
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">{{ $t->name }}</h4>
+                    <p class="card-text">
+                        Obtížnost:
+                        @if ($t->difficulty === 1)
+                            Lehká
+                        @elseif ($t->difficulty === 2)
+                            Střední
+                        @else
+                            Těžká
+                        @endif
+                    </p>
+                    <a href="{{ route('tests.take', ['id' => $t->id]) }}"
+                        class="btn btn-primary">Spustit test</a>
                 </div>
             </div>
         </div>
+    @endforeach
     </div>
 </div>
 @endsection
